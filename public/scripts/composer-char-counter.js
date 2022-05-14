3e39488b90ed5a14683a71c471b90803f3fc9fa1
Caitlin <cjcroteau@gmail.com>
2022-05-14 08:33:12 +0000

    add js to form and counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,27 @@
-console.log("I am being implemented")
+$(document).ready(function() {
+
+  $('#tweet-text').on("input", function() {
+    const maxCharacters = 140;
+    let characters = $(this).val();
+    let currentCharacters = maxCharacters - characters.length;
+
+    const parent = $("textarea").parent();
+    let counter = $(parent).find("output.counter");
+    counter.text(currentCharacters);
+
+    if (counter.text() < 0) {
+      counter.css("color", "red");
+    } else {
+      counter.css("color", "black");
+    }
+  });
+});
+
+
+// const tweetText = document.getElementById("tweet-text");
+
+// tweetText.addEventListener("input", (event) => {
+//   console.log(event);
+//   console.log("An input event has happened!");
+  
+// });
