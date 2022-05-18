22b0a895ecd61aef309a994b9fe2b77b6848200d
Caitlin <cjcroteau@gmail.com>
2022-05-18 06:24:02 +0000

    add ajax GET request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,50 +4,43 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// const { json } = require("express/lib/response");
+
 $(document).ready(function() {
 
 
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  const loadtweets = function() {
+    // console.log("render tweets",renderTweets)
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: "json"
+    }).then(function (tweets) {
+      console.log("success:", tweets);
+      renderTweets(tweets);
+    })
 
+  };
+
+  // loadtweets();
+  
   //event handler for submit action (related to the form element/not to the button element)
   const handleSubmit = event => {
     event.preventDefault();
-    //console.log($("#tweet-text").serialize())
     
     $.ajax({
       url: "/tweets",
       method: "POST",
       data: $("#tweet-text").serialize()
-    })
+    }).then(loadtweets)
   
   };
   
   //submit takes 'event' argument by default - don't have to explicitly write (event)
   $("#tweet-form").submit(handleSubmit);
+
+
+  
 
   // createTweetElement function: takes in a tweet object; returns a tweet <article> element containing the entire HTML structure of the tweet.
   const createTweetElement = function(tweet) {
@@ -90,9 +83,15 @@ $(document).ready(function() {
       const $tweet = createTweetElement(tweet);
       $('#tweets-container').append($tweet);
     }
+    
+    // console.log(document.getElementById("tweets-container").textContent)
   };
+
+
+
   
-  renderTweets(data)
+
+
 
 /*
 
@@ -118,3 +117,30 @@ $('#tweets-container').append($tweet); // to add it to the page so we can make s
 
 
 });
+
+
+// Fake data taken from initial-tweets.json
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
